d7b169eb50059a5dd16bcea412612d4e7573d861
bikkikumar0096 <bikkikumar0096@gmail.com>
2026-04-16 13:57:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAlert() {
+    alert("Thanks for reaching out, Bikki will contact you soon!");
+}
+
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
+}
